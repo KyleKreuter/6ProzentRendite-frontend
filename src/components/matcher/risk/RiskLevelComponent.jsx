320c64ab9897a08560f4fad7502e7dacb9baaf82
kylekreuter <kyle.kreuter@outlook.de>
2025-09-20 17:25:36 +0200

    "Enhance RiskLevelComponent with descriptive labels for risk levels below slider"

--- src/components/matcher/risk/RiskLevelComponent.jsx
+++ src/components/matcher/risk/RiskLevelComponent.jsx
@@ -32,21 +32,31 @@ const RiskLevelComponent = ({onRiskValueChange}) => {
                 <p className={"font-semibold"}>Risiko</p>
                 <RiskLevelPill riskLevel={getRiskLevelFromRiskValue(risk)}/>
             </div>
-            <Slider.Root
-                className="relative flex w-full touch-none select-none items-center"
-                value={[risk]}
-                onValueChange={(val) => setRisk(val[0])}
-                min={0}
-                max={4}
-                step={1}
-            >
-                <Slider.Track className="relative h-2 flex-1 rounded-full bg-gray-300">
-                    <Slider.Range
-                        className="absolute h-full rounded-full bg-gradient-to-r bg-pastel-cyan"/>
-                </Slider.Track>
-                <Slider.Thumb
-                    className="block h-4 w-4 rounded-full bg-pastel-blue focus:outline-none hover:cursor-pointer"/>
-            </Slider.Root>
+            <div className={"w-full relative"}>
+                <Slider.Root
+                    className="relative flex w-full touch-none select-none items-center"
+                    value={[risk]}
+                    onValueChange={(val) => setRisk(val[0])}
+                    min={0}
+                    max={4}
+                    step={1}
+                >
+                    <Slider.Track className="relative h-2 flex-1 rounded-full bg-gray-300">
+                        <Slider.Range
+                            className="absolute h-full rounded-full bg-gradient-to-r bg-pastel-cyan"/>
+                    </Slider.Track>
+                    <Slider.Thumb
+                        className="block h-4 w-4 rounded-full bg-pastel-blue focus:outline-none hover:cursor-pointer"/>
+                </Slider.Root>
+                <div className={"absolute -bottom-8 flex items-center justify-between w-full text-sm"}>
+                    <p className={"text-green-600"}>Sehr niedrig</p>
+                    <p className={"text-sky-600"}>Niedrig</p>
+                    <p className={"text-amber-600"}>Mittel</p>
+                    <p className={"text-red-600"}>Hoch</p>
+                    <p className={"text-red-600"}>Sehr hoch</p>
+                </div>
+            </div>
+
         </div>
     );
 };
