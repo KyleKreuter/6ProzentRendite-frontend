011425d44b660516d7e035c2f820aa61217cec25
kylekreuter <kyle.kreuter@outlook.de>
2025-11-25 10:01:04 +0100

    "Remove Login button from NavBarComponent to simplify layout"

--- src/components/navbar/NavBarComponent.jsx
+++ src/components/navbar/NavBarComponent.jsx
@@ -6,12 +6,6 @@ const NavBarComponent = () => {
     return (
         <div className={"h-16 flex items-center justify-between px-4"}>
                 <CompanyNameComponent/>
-            <div>
-                <button
-                    className={"rounded-lg border px-2 py-1 border-pastel-blue hover:border-pastel-cyan hover:cursor-pointer transform transition-all duration-200"}>
-                    <p className={"text-sm"}>Login</p>
-                </button>
-            </div>
         </div>
     );
 };
